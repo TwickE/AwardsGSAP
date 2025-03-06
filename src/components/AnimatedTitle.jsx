ff5849a1e1048dd691520342ca99a16491cf99da
TwickE <118013263+TwickE@users.noreply.github.com>
2025-03-06 16:52:05 +0000

    Started Working on the About Section

---
+++ src/components/AnimatedTitle.jsx
@@ -0,0 +1,11 @@
+const AnimatedTitle = () => {
+    return (
+        <p className="mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]">
+            Disc<b>o</b>ver the world's
+            <br />
+            l<b>a</b>rgest shared adventure
+        </p>
+    )
+}
+
+export default AnimatedTitle
